28165bee597f9cb3035f0672e19173e5caefa72e
GyebnarPeter <gyebnarpetter@gmail.com>
2020-05-25 15:01:08 +0200

    v1.1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+/* NAVIGATION
+---------------------------------------------------*/
 
+$('.nav-btn').click( () => {
+    $('.nav-wrap').slideToggle();
+});
+
+$('.nav-close-btn').click( () => {
+    $('.nav-wrap').slideToggle();
+});
+
+$('.home-btn').click( () => {
+    $('.home-submenu').slideToggle();
+});
