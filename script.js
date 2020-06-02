752c8d56b9cc6eddaeb788e4fe1c759dbde7b8c4
unknown <gyebnarpetter@gmail.com>
2020-06-02 13:59:51 +0200

    v1.1

--- script.js
+++ script.js
@@ -10,5 +10,42 @@ $('.nav-close-btn').click( () => {
 });
 
 $('.home-btn').click( () => {
+
+    ($('.home-btn svg').hasClass('menu-btn-toggle'))
+    ? $('.home-btn svg').removeClass('menu-btn-toggle')
+    : $('.home-btn svg').addClass('menu-btn-toggle')
+    
+
     $('.home-submenu').slideToggle();
 });
+
+$('.features-btn').click( () => {
+
+    ($('.features-btn svg').hasClass('menu-btn-toggle'))
+    ? $('.features-btn svg').removeClass('menu-btn-toggle')
+    : $('.features-btn svg').addClass('menu-btn-toggle')
+
+    $('.features-submenu').slideToggle();
+});
+
+$('.about-btn').click( () => {
+
+    ($('.about-btn svg').hasClass('menu-btn-toggle'))
+    ? $('.about-btn svg').removeClass('menu-btn-toggle')
+    : $('.about-btn svg').addClass('menu-btn-toggle')
+
+    $('.about-submenu').slideToggle();
+});
+
+$('.news-btn').click( () => {
+
+    ($('.news-btn svg').hasClass('menu-btn-toggle'))
+    ? $('.news-btn svg').removeClass('menu-btn-toggle')
+    : $('.news-btn svg').addClass('menu-btn-toggle')
+
+    $('.news-submenu').slideToggle();
+});
+
+
+/* SLIDER
+---------------------------------------------------*/
